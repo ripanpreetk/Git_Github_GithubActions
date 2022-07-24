bda46bb46c324a2f6933b62728eb967d491ab512
ripanpreetk <ripanpreetk@gmail.com>
2022-07-24 17:32:30 -0400

    Implemented add multiply subtract fns

--- testIntdataType.py
+++ testIntdataType.py
@@ -1,8 +1,16 @@
+'''apply Math functions'''
+def add(num1,num2):
+    '''adds 2 number'''
+    return num1+num2
 
-print("hello")
-number= input("Enter a number: ")
-try:
-    intnumber = int(number)
-    print(f"{number} is an integer")
-except:
-    print(f"{number} is not an integer")
+def mul(num1,num2):
+    '''multiply 2 number'''
+    return num1*num2
+
+def sub(num1,num2):
+    '''subtract 2 number'''
+    return num1-num2
+
+
+
+
