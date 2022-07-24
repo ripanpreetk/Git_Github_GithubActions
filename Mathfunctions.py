90724df193939cb15f98782a9131c16c3bf64fd5
ripanpreetk <ripanpreetk@gmail.com>
2022-07-24 17:39:50 -0400

    deleted Mathfunctions

---
+++ Mathfunctions.py
@@ -0,0 +1,16 @@
+'''apply Math functions'''
+def add(num1,num2):
+    '''adds 2 number'''
+    return num1+num2
+
+def mul(num1,num2):
+    '''multiply 2 number'''
+    return num1*num2
+
+def sub(num1,num2):
+    '''subtract 2 number'''
+    return num1-num2
+
+
+
+
